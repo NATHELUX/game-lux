19e355d3acf9a657cbfa968b8271c33b40b9441a
NATHELUX <matheus.lac.rod@gmail.com>
2026-07-21 00:45:45 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const { Client, GatewayIntentBits } = require("discord.js");
 
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds]
+  intents: [GatewayIntentBits.Guilds],
 });
 
 client.once("ready", () => {
   console.log(`Bot online: ${client.user.tag}`);
 });
 
-client.login("COLOQUE_SEU_TOKEN_AQUI");
+client.login(process.env.TOKEN);
