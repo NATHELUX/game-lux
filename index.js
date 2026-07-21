785e1b1a0bf404f960f989b88b3fd8090019edb6
NATHELUX <matheus.lac.rod@gmail.com>
2026-07-21 00:47:22 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const { Client, GatewayIntentBits } = require("discord.js");
-
-const client = new Client({
-  intents: [GatewayIntentBits.Guilds],
-});
-
-client.once("ready", () => {
-  console.log(`Bot online: ${client.user.tag}`);
-});
-
-client.login(process.env.TOKEN);
+{
+  "name": "jogo-lux",
+  "version": "1.0.0",
+  "description": "Bot de moderação",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "discord.js": "^14.21.0"
+  }
+}
